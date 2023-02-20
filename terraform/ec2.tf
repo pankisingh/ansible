4fc50a07fbc008d9e3fc756b7491a668c7198c67
Pankaj Singh <Pankisingh070@gmail.com>
2023-02-20 23:54:39 +0530

    updated

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,23 +1,33 @@
-resource "aws_spot_instance_request" "cheap_worker" {
-  count                     = local.LENGTH
-  //count                     = length(var.COMPONENTS)
-  ami                       = "ami-0bb6af715826253bf"
-  spot_price                = "0.0031"
-  instance_type             = "t3.micro"
-  vpc_security_group_ids    = ["sg-0017bec6ae883767e"]
-  wait_for_fulfillment      = true
-  //spot_type                 = "persistent"
-  tags                      = {
-    Name                    = element(var.COMPONENTS, count.index)
+# resource "aws_spot_instance_request" "cheap_worker" {
+#   count                     = local.LENGTH
+#   //count                     = length(var.COMPONENTS)
+#   ami                       = "ami-0bb6af715826253bf"
+#   spot_price                = "0.0031"
+#   instance_type             = "t3.micro"
+#   vpc_security_group_ids    = ["sg-0017bec6ae883767e"]
+#   wait_for_fulfillment      = true
+#   //spot_type                 = "persistent"
+#   tags                      = {
+#     Name                    = element(var.COMPONENTS, count.index)
+#   }
+# }
+
+resource "aws_instance" "db-instances" {
+  count                       = length(var.DB_COMPONENTS)
+ ami                          = "ami-0bb6af715826253bf"
+  instance_type               = "t3.micro"
+  vpc_security_group_ids      = ["sg-0017bec6ae883767e"]
+  tags                        = {
+    Name                      = "${element(var.DB_COMPONENTS, count.index)}-${var.ENV}"
   }
 }
 
-resource "aws_ec2_tag" "name-tag" {
-  count                     = local.LENGTH
-  resource_id               = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index)
-  key                       = "Name"
-  value                     = element(var.COMPONENTS, count.index)
-}
+# resource "aws_ec2_tag" "name-tag" {
+#   count                     = local.LENGTH
+#   resource_id               = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index)
+#   key                       = "Name"
+#   value                     = element(var.COMPONENTS, count.index)
+# }
 
 
 resource "aws_route53_record" "records" {
